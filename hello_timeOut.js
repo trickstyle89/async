6ee2c1b11b006f8d2408b4949a5585a364994832
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-02-14 20:55:32 -0500

    late night tuesday

--- hello_timeOut.js
+++ hello_timeOut.js
@@ -1,41 +1,16 @@
-const sentence = "hello there from lighthouse labs";
+const messages = ['4311o', 'th3r3', 'w0r1d'];
 
-let currentLetterIndex = 0;
-let currentCharIndex = 0;
-let intervalTime;
-
-
-const printMessage = () => {
-  const letter = sentence[currentLetterIndex];
-  process.stdout.write(letter[currentCharIndex]);
-
-  currentCharIndex++;
-
-  // checks to see if the letter is finished interating.
-  if (currentCharIndex === letter.length) {
-    currentLetterIndex++;
-    currentCharIndex = 0;
-  }
-
-    // checks to see if the whole sentence is finished interating.
-  if (currentLetterIndex === sentence.length) {
-    clearInterval(intervalTime);
-    console.log('\n');
-  }
+const printMessage = (messages) => {
+  let i = 0;
+  const intervalId = setInterval(() => {
+    process.stdout.write(messages[i]);
+    i++;
+    if (i >= messages.length) {
+      clearInterval(intervalId);
+    }
+  }, 50);
 };
 
-//callback putting the entire function into setInterval;
-intervalTime = setInterval(printMessage, 50);
 
-/*
-The currentLetterIndex variable keeps track of the current letter being printed.
- When printMessage is called, it gets the word at the current index from the word array,
-and then prints the character at the current index in that message.
 
-After printing the character, printMessage increments the currentCharIndex variable,
- which keeps track of the index of the next character to print in the current word. 
- When currentCharIndex reaches the length of the current word, that means the entire word
-  has been printed, and so currentLetterIndex is incremented to move on to the next letter in 
-  the sentence, and currentCharIndex is reset to 0 to start printing the first 
-  character of the next letter.
-  */
+printMessage(messages);
--- timer/timer1.js
+++ timer/timer1.js
@@ -1,4 +1,24 @@
-process.stdout.write('\x07');
+const [node, script, ...times] = process.argv;
+
+
+// created a function its own to call when needed.
+const beep = () => {
+  process.stdout.write('\x07'); 
+  // this is the sound FX provided by LHL
+};
+
+const setTimers = (times) => {
+  times
+    .map(Number) //need to ensure it is a number if it s inputted as a string.
+    .filter(time => !isNaN(time)) //filtering out SOME edge cases with isNaN(time). Super useful code.
+    .sort((a, b) => a - b) // sorting the numbers to ensure the alarm is set in order.
+    .forEach((time, i) => { // this is the part where the alarm is set for each time
+      setTimeout(beep, time * 1000 + i * 1000); 
+      // extra 1000 is added to make up for the delay from first beep.
+    });
+};
+
+setTimers(times);
 
 /* node timer1.js 10 3 5 15 9 
 
