2f9a4658cfcab032a5b063877ceac4cedbabfebd
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-02-15 11:15:57 -0500

    wednesday

--- networking/net_example/client.js
+++ networking/net_example/client.js
@@ -1,8 +1,16 @@
 const net = require("net");
 
 const conn = net.createConnection({
-  host: "SERVER IP ADDRESS HERE", // change to IP address of computer, more on that below
+  host: 'localhost', // change to IP address of computer, more on that below
   port: 3000,
 });
 
 conn.setEncoding("utf8"); // interpret data as text
+
+conn.on("data", (data) => {
+  console.log("Server says: ", data);
+});
+
+conn.on("connect", () => {
+  conn.write("Hello from client!");
+});
