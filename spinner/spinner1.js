a980b1b2b340001231cfdd53cbb95d3c29ced449
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-02-14 19:23:52 -0500

    Update spinner1.js

--- spinner/spinner1.js
+++ spinner/spinner1.js
@@ -2,17 +2,38 @@ process.stdout.write('hello from spinner1.js... \rheyyy\n');
 
 setTimeout(() => {
   process.stdout.write('\r|   ');
-}, 100);
+}, 250);
 
 setTimeout(() => {
   process.stdout.write('\r/   ');
-}, 300);
+}, 500);
 
 setTimeout(() => {
   process.stdout.write('\r-   ');
-}, 500);
+}, 750);
 
 setTimeout(() => {
   // Need to escape the backslash since it's a special character.
   process.stdout.write('\r\\   '); 
-}, 700);
+}, 1000);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 1250);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1500);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1750);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 2000);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 2250);
