dfcfa34e3176dc4e8810af1b5cc2e378a3b14234
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-02-14 19:55:43 -0500

    tuesday

--- spinner/spinner1.js
+++ spinner/spinner1.js
@@ -14,7 +14,7 @@ setTimeout(() => {
 
 setTimeout(() => {
   // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   '); 
+  process.stdout.write('\r\\   ');
 }, 1000);
 
 setTimeout(() => {
@@ -31,7 +31,7 @@ setTimeout(() => {
 
 setTimeout(() => {
   // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   '); 
+  process.stdout.write('\r\\   ');
 }, 2000);
 
 setTimeout(() => {
---
+++ spinner/spinner2.js
@@ -0,0 +1,19 @@
+const animationSequences = [  '\r|   ',  '\r/   ',  '\r-   ',  '\r\\   ',  '\r|   ',];
+
+let iterationCount = 0; // Keep track of the number of iterations
+
+const intervalId = setInterval(() => {
+    // the modulus operator is used to keep animation running by ensuring the index limit count.
+  const sequence = animationSequences[iterationCount % animationSequences.length];
+  process.stdout.write(sequence);
+
+  iterationCount++;
+
+  // Check if we've reached the maximum number of iterations
+  if (iterationCount === 4 * 4) { // 4 is the number of animation frames
+    clearInterval(intervalId);
+    process.stdout.write('\n');
+  }
+}, 250);
+
+process.stdout.write('hello from spinner2.js... \rheyyy\n');
